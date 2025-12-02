bac6e11b1a6019ad3e995773f0f87d9e36807e00
DmitryAkh <165409513+DmitryAkh@users.noreply.github.com>
2025-12-02 18:05:10 +0300

    Реализован DataStore

--- app/src/main/java/com/dakh/newsprism/domain/entity/Settings.kt
+++ app/src/main/java/com/dakh/newsprism/domain/entity/Settings.kt
@@ -4,8 +4,15 @@ data class Settings(
     val language: Language,
     val interval: Interval,
     val notificationsEnabled: Boolean,
-    val wifiOnly: Boolean
-    )
+    val wifiOnly: Boolean,
+) {
+    companion object {
+        val DEFAULT_LANGUAGE = Language.ENGLISH
+        val DEFAULT_INTERVAL = Interval.MIN_15
+        const val DEFAULT_NOTIFICATIONS_ENABLED = false
+        const val DEFAULT_WIFI_ONLY = false
+    }
+}
 
 enum class Language {
 
